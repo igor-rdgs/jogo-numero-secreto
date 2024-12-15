215e5716f4b7b4d58fd8555915b326f2a2efcd36
Igor Rodrigues <igor.aguiar1@live.com>
2024-12-14 23:29:56 -0300

    Alterando título

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function mensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Descubra o número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 mensagemInicial();
